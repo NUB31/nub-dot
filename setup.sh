f5d502fe3fe30a699ff8945f0839500595359e7b
nub31 <oliverlhs@outlook.com>
2024-05-26 20:47:37 +0200

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+# Install starship
+curl -sS https://starship.rs/install.sh | sh
+
+# Install stow
+sudo apt update
+sudo apt install stow -y
+
+# Create symlinks between parents and here
+stow --adopt .
+
+# Restore merged symlinks
+git restore . 
