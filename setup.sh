8383b9745ef09ad9a2bce911b94d05f966a5a477
nub31 <oliverlhs@outlook.com>
2024-12-20 19:18:19 +0100

    tset

--- setup.sh
+++ setup.sh
@@ -2,12 +2,7 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 
-# Install prefrerred packages
-sudo apt purge  grim stow slurp cliphist sway wofi nwg-displays nwg-look hyprland dunst pavucontrol pamixer thunar libhyprutils-dev libhyprlang-dev -y
-
-# Set up required directories
-mkdir -p "$HOME/screenshots"
-
-sudo rm /bin/hypr*
+stow --adopt .
+git restore --hard
 
 sudo apt autoremove
