949ba8c7d73801ae604627ce3598b230c6e5c227
nub31 <oliverlhs@outlook.com>
2024-12-20 21:03:31 +0100

    ...

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get -y install stow git sway swayidle swaylock fuzzel
+sudo apt-get -y install stow git thunar firefox-esr sway swayidle swaylock fuzzel
 sudo apt autoremove
 
 stow --adopt .
